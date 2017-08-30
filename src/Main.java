6f6527fd33ecbc7b0fca63af630457e451487183
shubhamPrakashJha <sjonline4u@gmail.com>
2017-08-30 13:42:57 +0530

    new comments for use of scanner and use of advanced for loop

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
+//Scanner class imported here
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner created below
+        //Scanner object  created below
         Scanner input=new Scanner(System.in);
 
         int[] array=new int[3];
-        // taking input using for loop here
         for(int x=0;x<3;x++){
+            //scanner method for getting integer implemented
             array[x]=input.nextInt();
         }
 
 
         int sum=0;
-        for(int x=0;x<array.length;x++){
-            sum+=array[x];
+        for(int x: array){
+            sum+= x;
         }
 
         System.out.println(sum);
